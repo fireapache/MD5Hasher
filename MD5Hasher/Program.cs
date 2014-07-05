5733ac327413a81a0c406c382a5affad33b61de0
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2014-07-05 10:09:29 -0300

    First implementation

--- MD5Hasher/Program.cs
+++ MD5Hasher/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace MD5Hasher
 {
@@ -10,7 +11,23 @@ namespace MD5Hasher
     {
         static void Main(string[] args)
         {
+            String str;
+            byte[] hash;
+            MD5 hasher;
 
+            str = Console.ReadLine();
+            hasher = MD5.Create();
+            hash = Encoding.UTF8.GetBytes(str);
+            hash = hasher.ComputeHash(hash);
+
+            str = "";
+
+            foreach (byte b in hash)
+            {
+                str = str + b.ToString("x2");
+            }
+
+            Console.WriteLine(str);
         }
     }
 }
